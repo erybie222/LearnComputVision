2dc24ce84fdf64709ec7037daf0f7c7a4fa0a1a3
erybie222 <erybie222@gmail.com>
2025-12-23 18:47:09 +0100

    finger counter and volume hand control

--- HandTracking/HandTrackingModule.py
+++ HandTracking/HandTrackingModule.py
@@ -5,13 +5,19 @@ class HandDetector:
     def __init__(self, mode=False, maxHands=2, detectionCon=0.5, trackCon=0.5):
         self.mode = mode
         self.maxHands = maxHands
-        self.decisionCon = detectionCon
+        self.detectionCon = detectionCon
         self.trackCon = trackCon
-
         self.mpHands = mp.solutions.hands
-        self.hands = self.mpHands.Hands(self.mode, self.maxHands, self.decisionCon, self.trackCon)
-        self.mpDraw = mp.solutions.drawing_utils
 
+
+        self.hands = self.mpHands.Hands(
+            static_image_mode=self.mode,
+            max_num_hands=self.maxHands,
+            min_detection_confidence=self.detectionCon,
+            min_tracking_confidence=self.trackCon
+        )
+
+        self.mpDraw = mp.solutions.drawing_utils
     def findHands(self, img, draw = True):
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         self.results = self.hands.process(imgRGB)
